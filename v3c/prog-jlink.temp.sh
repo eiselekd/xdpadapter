0bb15a756933444c40042d58eb4b976a3b0f04df
Konrad Eisele <eiselekd@gmail.com>
2016-07-09 01:01:31 +0200

    all

---
+++ v3c/prog-jlink.temp.sh
@@ -0,0 +1,4 @@
+cable ft2232 pid=0x6010 vid=0x0403 interface=1
+bsdl path --curdir--/Programming/bsdl
+detect
+svf --curdir--/Programming/svf/bbv3.svf progress stop
---
+++ v3c/prog-jtagkey.temp.sh
@@ -0,0 +1,4 @@
+cable ft2232 pid=0x6010 vid=0x0403 interface=1
+bsdl path --curdir--/Programming/bsdl
+detect
+svf --curdir--/BBv2-JTAGkey-v1.4-VHDL.svf progress stop
